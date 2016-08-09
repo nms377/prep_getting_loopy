a53024de12997c2d1ae2e4ceea88b2d34e0fcbf6
nms377 <nms377@nyu.edu>
2016-08-08 21:13:21 -1000

    created a for loop listing presidents

--- loops.js
+++ loops.js
@@ -1,6 +1,8 @@
 /*## `Presidents - Accessing values in an Array`
 Declare a variable named `presidents` which contains the first 5 presidents' last names: `Washington`, `Adams`, `Jefferson`, `Madison`, `Monroe`.*/
 
+var presidents = ['Washington', 'Adams', 'Jefferson', 'Madison', 'Monroe']; 
+
 /*Now write a _FOR_ loop which iterates through this Array.
 
 Within the _FOR_ loop, use `console.log` to:
@@ -9,9 +11,13 @@ Within the _FOR_ loop, use `console.log` to:
 - Log what is at index `i` in the array. Use a message like `Value at Index is: ` and append the value stored within the Array at that index.*/
 
 /***bonus:** could you take the code inside of this FOR loop and encapsulate it inside of a function called `printContent` and still achieve the same result? This new function should accept two parameters.*/
+function presidentsNames (names){
+	for (var i = 0; i < names.length; i++){
+		console.log(names[i]);
+	}
+}
 
-
-
+presidentsNames(presidents);
 
 
 /*Declare a variable named `stringOfNumbers` and set it's value to `''` (an empty String).*/
